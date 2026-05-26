7b3aa6d5a35f93b20970cb8bf3bfa2164ae633b6
Tejas pandav <99353991+Tejas-SOC@users.noreply.github.com>
2026-05-27 02:25:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         resume: () => {
             const link = document.createElement("a");
-            link.href = "/Tejas Pandav SOC Analyst.pdf";
+            link.href = "Tejas Pandav SOC Analyst.pdf";
             link.download = "Tejas Pandav SOC Analyst.pdf";
             document.body.appendChild(link);
             link.click();
