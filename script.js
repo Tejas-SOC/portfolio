9d5c85480f71669d61379b8e3efd8f3503da48fd
Tejas pandav <99353991+Tejas-SOC@users.noreply.github.com>
2026-05-27 02:37:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <b>Phone &nbsp;&nbsp;&nbsp;:</b> <a href="tel:+91 XXXXX XXXXX" class="custom-link">+91 9021451291</a><br>
         <b>Email &nbsp;&nbsp;&nbsp;:</b> <a href="mailto:tejaspandav248@gmail.com" class="custom-link">tejaspandav248@gmail.com</a><br>
         <b>LinkedIn :</b> <a href="https://www.linkedin.com/in/tejas-pandav-soc/" target="_blank" class="custom-link">linkedin.com/tejas-pandav-soc/</a><br>
-        <b>GitHub &nbsp;&nbsp;:</b> <a href="https://github.com/Tejas-SOC" target="_blank" class="custom-link">tejas248</a><br>
+        <b>GitHub &nbsp;&nbsp;:</b> <a href="https://github.com/Tejas-SOC" target="_blank" class="custom-link">Tejas-SOC</a><br>
             `;
         },
 
